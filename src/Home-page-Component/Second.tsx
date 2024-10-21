5e355395f13423bc10cfe8fd9941e537352921da
gitgauravsingh <gauravsurvanshi2002@gmail.com>
2024-10-21 14:43:04 +0530

    message

--- src/Home-page-Component/Second.tsx
+++ src/Home-page-Component/Second.tsx
@@ -22,26 +22,28 @@ const Second: React.FC<ISecond> = ({ second, setSecond }) => {
         <>
           {second && (
             <div className={Style.seconddata}>
-              <div className={Style.wrapper}>
-                <div className={Style.dotdatabar}> </div>
-                <div className={Style.dotbar}>
-                  <span>
-                    <img
-                      src="https://dvf7opio6knc7.cloudfront.net/satyugImages/game-archery-2.png"
-                      alt=""
-                    />
-                  </span>
-                  <div className={Style.imgdata}>
-                    <img
-                      src="https://dvf7opio6knc7.cloudfront.net/collective_p2/Ram%20Mandir%20ka%20Nirmaan.png"
-                      alt=""
-                    />
-                  </div>
-                  <div className={Style.button} onClick={handleVedioClick}>
-                    <a href="#">Enter</a>
-                  </div>
-                  <div className={Style.text}>
-                    <h2>Ram Mandir Ka Nirman</h2>
+              <div className={Style.overlaydata}>
+                <div className={Style.wrapper}>
+                  <div className={Style.dotdatabar}></div>
+                  <div className={Style.dotbar}>
+                    <span>
+                      <img
+                        src="https://dvf7opio6knc7.cloudfront.net/satyugImages/game-archery-2.png"
+                        alt=""
+                      />
+                    </span>
+                    <div className={Style.imgdata}>
+                      <img
+                        src="https://dvf7opio6knc7.cloudfront.net/collective_p2/Ram%20Mandir%20ka%20Nirmaan.png"
+                        alt=""
+                      />
+                    </div>
+                    <div className={Style.button} onClick={handleVedioClick}>
+                      <a href="#">Enter</a>
+                    </div>
+                    <div className={Style.text}>
+                      <h2>Ram Mandir Ka Nirman</h2>
+                    </div>
                   </div>
                 </div>
               </div>
